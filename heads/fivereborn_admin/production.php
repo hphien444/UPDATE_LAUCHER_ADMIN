9980ce20838939d836ae23fb6faf5de89d45e271
hphien444 <hphien444@gmail.com>
2026-06-23 16:03:50 +0700

    Update admin anticheat DLL

--- heads/fivereborn_admin/production.php
+++ heads/fivereborn_admin/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000024');
 header('x-amz-meta-bootstrap-size: 5094848');
 header('x-amz-meta-bootstrap-object: 975cd3c5889d1f222a723ba7a9a645fc9266cda5881b8352c8b20f243ab9496b');
-header('x-amz-meta-branch-version: 1782130943');
-header('x-amz-meta-branch-manifest: f451f27826ac1f4c34845bb4f3051956d689922a9731dfc4f312d42ff880fb42');
+header('x-amz-meta-branch-version: 1782205372');
+header('x-amz-meta-branch-manifest: 0bad7cb75c2ec42915d770b5021cd9d53e108e7e54384beee068a3599e6aac2d');
 echo "OK";
 ?>
