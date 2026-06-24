8abef949d5987b6287a2f50ac1226b85dce9fd25
hphien444 <hphien444@gmail.com>
2026-06-24 18:00:10 +0700

    Back admin cursor logic to source

--- heads/fivereborn_admin/production.php
+++ heads/fivereborn_admin/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000024');
 header('x-amz-meta-bootstrap-size: 5094848');
 header('x-amz-meta-bootstrap-object: 975cd3c5889d1f222a723ba7a9a645fc9266cda5881b8352c8b20f243ab9496b');
-header('x-amz-meta-branch-version: 1782298160');
-header('x-amz-meta-branch-manifest: 2ed25892d6d1fcd5540c5e86f5771fc38344d3b2f80319f1e457d738735c5f40');
+header('x-amz-meta-branch-version: 1782298746');
+header('x-amz-meta-branch-manifest: 0b19842c5c5d8c8e044c2e26b3662fceb200d0e957008268d45530d942e9cefc');
 echo "OK";
 ?>
