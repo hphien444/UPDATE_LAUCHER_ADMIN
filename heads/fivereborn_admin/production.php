79770d2ca0d635c4149e6cbdc46c7d4fbea8d34b
hphien444 <hphien444@gmail.com>
2026-06-24 17:50:10 +0700

    Restore admin cursor-only NUI mode

--- heads/fivereborn_admin/production.php
+++ heads/fivereborn_admin/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000024');
 header('x-amz-meta-bootstrap-size: 5094848');
 header('x-amz-meta-bootstrap-object: 975cd3c5889d1f222a723ba7a9a645fc9266cda5881b8352c8b20f243ab9496b');
-header('x-amz-meta-branch-version: 1782297209');
-header('x-amz-meta-branch-manifest: b141ee25e2f70086085d093ca883df696aa2092bd7f86165139812cb0c09a02c');
+header('x-amz-meta-branch-version: 1782298160');
+header('x-amz-meta-branch-manifest: 2ed25892d6d1fcd5540c5e86f5771fc38344d3b2f80319f1e457d738735c5f40');
 echo "OK";
 ?>
