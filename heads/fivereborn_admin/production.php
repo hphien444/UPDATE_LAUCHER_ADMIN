6b18e5ff276e0d38d9744946ff0a9d69fe0ca313
hphien444 <hphien444@gmail.com>
2026-06-24 17:34:29 +0700

    Harden admin NUI focus cursor state

--- heads/fivereborn_admin/production.php
+++ heads/fivereborn_admin/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000024');
 header('x-amz-meta-bootstrap-size: 5094848');
 header('x-amz-meta-bootstrap-object: 975cd3c5889d1f222a723ba7a9a645fc9266cda5881b8352c8b20f243ab9496b');
-header('x-amz-meta-branch-version: 1782296173');
-header('x-amz-meta-branch-manifest: 61a8f5917f1eb8c3265c8a864e0bf03a75ea981c94c4be563732681c632f4a71');
+header('x-amz-meta-branch-version: 1782297209');
+header('x-amz-meta-branch-manifest: b141ee25e2f70086085d093ca883df696aa2092bd7f86165139812cb0c09a02c');
 echo "OK";
 ?>
