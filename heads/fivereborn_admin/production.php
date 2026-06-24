8da6b594645891c6d1446f6e85ec72db31315ce5
hphien444 <hphien444@gmail.com>
2026-06-24 14:21:41 +0700

    Add NUI focus release watchdog

--- heads/fivereborn_admin/production.php
+++ heads/fivereborn_admin/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000024');
 header('x-amz-meta-bootstrap-size: 5094848');
 header('x-amz-meta-bootstrap-object: 975cd3c5889d1f222a723ba7a9a645fc9266cda5881b8352c8b20f243ab9496b');
-header('x-amz-meta-branch-version: 1782284667');
-header('x-amz-meta-branch-manifest: 6219b8e4b317b52c69e390f665a1f93bd91ab827a2f119e49bbd95e6e4b4a00d');
+header('x-amz-meta-branch-version: 1782285641');
+header('x-amz-meta-branch-manifest: 7686d4a38d51c26e6cd8a3f42977daedbf95e31073545bed3075b1d4e23e8e7f');
 echo "OK";
 ?>
