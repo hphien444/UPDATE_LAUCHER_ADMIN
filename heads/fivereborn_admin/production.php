e8dd21643d1e0cac8cd86e2f1e88a46732c79fb7
hphien444 <hphien444@gmail.com>
2026-06-27 17:52:49 +0700

    Update admin package: Alt+F4 disconnect before exit

--- heads/fivereborn_admin/production.php
+++ heads/fivereborn_admin/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000024');
 header('x-amz-meta-bootstrap-size: 5094848');
 header('x-amz-meta-bootstrap-object: 975cd3c5889d1f222a723ba7a9a645fc9266cda5881b8352c8b20f243ab9496b');
-header('x-amz-meta-branch-version: 1782298746');
-header('x-amz-meta-branch-manifest: 0b19842c5c5d8c8e044c2e26b3662fceb200d0e957008268d45530d942e9cefc');
+header('x-amz-meta-branch-version: 1782557450');
+header('x-amz-meta-branch-manifest: f780bc3d81071041a1bda4193de34c2ca8e620fdcc2c2dd53a630f15e2c7570a');
 echo "OK";
 ?>
