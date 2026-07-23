9cf5f005827c2c0a8b63f548166eeaa7331f2766
HIEN <hphien444@gmail.com>
2026-07-23 12:05:15 +0700

    Restore latest admin launcher UI assets

--- heads/fivereborn_admin/production.php
+++ heads/fivereborn_admin/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000034');
 header('x-amz-meta-bootstrap-size: 5098904');
 header('x-amz-meta-bootstrap-object: eb231ddedb4f9dce3bc3906c9a1e5b2df6d1c52b90026944ea093d17cfd60b88');
-header('x-amz-meta-branch-version: 1784781700');
-header('x-amz-meta-branch-manifest: 21b7706868bf5c916386015df53fa3498cfbfd9cfb62b4a2710f7e9c3eb15cb0');
+header('x-amz-meta-branch-version: 1784783067');
+header('x-amz-meta-branch-manifest: 13ea8bc262784149607848ac9891f44d5fda78be271242d897dc7dff6f44dd02');
 echo "OK";
 ?>
