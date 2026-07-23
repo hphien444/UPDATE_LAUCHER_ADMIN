cdfa7cc1e20d33e260f5c27f845e79bbc19f4c59
HIEN <hphien444@gmail.com>
2026-07-23 11:43:05 +0700

    Publish admin launcher anticheat update 1000034

--- heads/fivereborn_admin/production.php
+++ heads/fivereborn_admin/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000033');
-header('x-amz-meta-bootstrap-size: 5099364');
-header('x-amz-meta-bootstrap-object: 5975194f2fe303a3c690aeb6f39f7de228634c675c04dfaa73f06681609403e7');
-header('x-amz-meta-branch-version: 1784728597');
-header('x-amz-meta-branch-manifest: b0350b381a42826abfd59606c56f7892bda9e634cfac733fd06633922ad9af15');
+header('x-amz-meta-bootstrap-version: 1000034');
+header('x-amz-meta-bootstrap-size: 5098904');
+header('x-amz-meta-bootstrap-object: eb231ddedb4f9dce3bc3906c9a1e5b2df6d1c52b90026944ea093d17cfd60b88');
+header('x-amz-meta-branch-version: 1784781700');
+header('x-amz-meta-branch-manifest: 21b7706868bf5c916386015df53fa3498cfbfd9cfb62b4a2710f7e9c3eb15cb0');
 echo "OK";
 ?>
