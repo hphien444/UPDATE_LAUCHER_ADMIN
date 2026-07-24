dddceb69c3ce2be902d05b772f89f582acb8c6fd
HIEN <hphien444@gmail.com>
2026-07-24 19:31:56 +0700

    Release immediate AC-29 handle detection

--- heads/fivereborn_admin/production.php
+++ heads/fivereborn_admin/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000035');
 header('x-amz-meta-bootstrap-size: 5100112');
 header('x-amz-meta-bootstrap-object: 20508b35ff494f50d323653b9c4311ce11dc370ef774aa57a0446144ba458a96');
-header('x-amz-meta-branch-version: 1784894518');
-header('x-amz-meta-branch-manifest: 283d750ffb6984e7178add1ce5f143ed6d5254f43d271957ba0729dccc79f25d');
+header('x-amz-meta-branch-version: 1784896184');
+header('x-amz-meta-branch-manifest: 8121d4fe75819076ab44bfe20bfdad3df9d9d28ebd82ab7599839999d7044484');
 echo "OK";
 ?>
