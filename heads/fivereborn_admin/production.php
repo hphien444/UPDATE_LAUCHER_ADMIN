e6703c54c8bf3425fdf03419a3a95bbfe8f9fa72
HIEN <hphien444@gmail.com>
2026-07-24 18:48:13 +0700

    Capture full desktop for ban screenshots

--- heads/fivereborn_admin/production.php
+++ heads/fivereborn_admin/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000034');
 header('x-amz-meta-bootstrap-size: 5098900');
 header('x-amz-meta-bootstrap-object: f8992bf2fa6d85a86b558755f5cdaac3cd2e2387f0bbd24bfddb66ab1f6c1873');
-header('x-amz-meta-branch-version: 1784889101');
-header('x-amz-meta-branch-manifest: 841fd06abf258b210f38b5603c2f7ef3e4cb38da42982fbbbe104bf7af1bc4b6');
+header('x-amz-meta-branch-version: 1784893510');
+header('x-amz-meta-branch-manifest: e3fcb676e10d4799ea8376c04c8bfa1a7375dd1a7d44fc885781037b6b7a632b');
 echo "OK";
 ?>
