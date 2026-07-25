72b74b8563415a754fdf69c0f91d9e70b4d8ada5
HIEN <hphien444@gmail.com>
2026-07-25 18:16:29 +0700

    Release admin launcher bootstrap 1000037

--- heads/fivereborn_admin/production.php
+++ heads/fivereborn_admin/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000036');
-header('x-amz-meta-bootstrap-size: 5099904');
-header('x-amz-meta-bootstrap-object: 09f762294a0224eee40078bcb4723d4bc0aaee57ddd66683ce5ff1771350d086');
-header('x-amz-meta-branch-version: 1784973976');
+header('x-amz-meta-bootstrap-version: 1000037');
+header('x-amz-meta-bootstrap-size: 5100424');
+header('x-amz-meta-bootstrap-object: a262460fa9880a89781ea0507444cf0bdf447eba9677fea4c655e5cf3ba898c7');
+header('x-amz-meta-branch-version: 1784977969');
 header('x-amz-meta-branch-manifest: 52d1eb5a768cb6698108da83014ebe2ddb0cf20769ac81830d9e302f25d00231');
 echo "OK";
 ?>
