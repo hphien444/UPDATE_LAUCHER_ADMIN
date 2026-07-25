b52a4f171b29ccf47492b6b5951de2930423c7f2
HIEN <hphien444@gmail.com>
2026-07-25 14:50:29 +0700

    Publish clean admin glue build update

--- heads/fivereborn_admin/production.php
+++ heads/fivereborn_admin/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000035');
 header('x-amz-meta-bootstrap-size: 5100160');
 header('x-amz-meta-bootstrap-object: e55a67c3527c093a48121ff1e5cc15b510db1b5392bdf3baa2e83081550a9937');
-header('x-amz-meta-branch-version: 1784963338');
-header('x-amz-meta-branch-manifest: 319484395b92272fa5d6303dbad08bad60684ae9132911e4eb2fbbb95de1d0d8');
+header('x-amz-meta-branch-version: 1784965702');
+header('x-amz-meta-branch-manifest: 7c362fb496803e1113b97a21d6a8578432740823bdf27525c4e39a03e6d99f69');
 echo "OK";
 ?>
