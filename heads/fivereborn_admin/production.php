f4486d7d92cbbd10992f99f19f419d684cbb894f
HIEN <hphien444@gmail.com>
2026-08-14 10:50:32 +0700

    Fix hidden game data prompt in Admin 1000071

--- heads/fivereborn_admin/production.php
+++ heads/fivereborn_admin/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000070');
-header('x-amz-meta-bootstrap-size: 5108672');
-header('x-amz-meta-bootstrap-object: 88a6d0da5b75aa1980e1f427e2300057fec4fd46e3d838bea84b5470aad2787d');
-header('x-amz-meta-branch-version: 1786677042');
+header('x-amz-meta-bootstrap-version: 1000071');
+header('x-amz-meta-bootstrap-size: 5109528');
+header('x-amz-meta-bootstrap-object: 41d0e5ecc8d41b0323cf74b5ff395d59e5648efd36963b5cbe9003171e050578');
+header('x-amz-meta-branch-version: 1786678988');
 header('x-amz-meta-branch-manifest: 96ca9ca351c26e2a4253c43b557c9d929335c8b6cadd3f41c8c3d49d462980f8');
 echo "OK";
 ?>
